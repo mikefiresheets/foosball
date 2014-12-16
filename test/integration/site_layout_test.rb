c0f8c5752360f86a254a755799b1b5e7da507a3f
Mike Firesheets <mike@mighty-mustache.com>
2014-12-16 15:40:56 -0500

    Sign Up page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout header links" do
+  test "layout links" do
     get root_path
     assert_template 'static_pages/home'
+    # header links
     assert_select "a[href=?]", root_path
-  end
 
-  test "layout footer links" do
-    get root_path
-    assert_template 'static_pages/home'
+    # footer links
     assert_select 'a[href=?]', help_path
+
+    get signup_path
+    assert_select 'title', full_title('Sign Up')
   end
 end
