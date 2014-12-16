fa708dc56e7b71129f60e74e52631b42f5c24d27
Mike Firesheets <mike@mighty-mustache.com>
2014-12-16 16:06:15 -0500

    Add unique index to User email to enfore...uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216204332) do
+ActiveRecord::Schema.define(version: 20141216210515) do
 
   create_table "users", force: true do |t|
     t.string   "first"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20141216204332) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
