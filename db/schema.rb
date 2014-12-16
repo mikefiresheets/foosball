b81c052b0b74027dcfba546a3e55d7993adc8b50
Mike Firesheets <mike@mighty-mustache.com>
2014-12-16 15:49:00 -0500

    Rails generation User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141216204332) do
+
+  create_table "users", force: true do |t|
+    t.string   "first"
+    t.string   "last"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
