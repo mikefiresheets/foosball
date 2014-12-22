a7f50a4723a47b2ae2f12582a1d9993f1620bc4a
Mike Firesheets <mike@mighty-mustache.com>
2014-12-22 15:04:58 -0500

    Remember me tests passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.find_by(email: params[:session][:email].downcase).try(:authenticate, params[:session][:password])
-      log_in user
-      remember user
-      redirect_to user
+    if @user = User.find_by(email: params[:session][:email].downcase).try(:authenticate, params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:alert] = 'Invalid email/password combination.'
       render 'new'
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
