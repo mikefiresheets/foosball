a4e5a96bdf50bc2ba4bd0aab97885ceb22ca4f5b
Mike Firesheets <mike@mighty-mustache.com>
2015-01-09 11:59:42 -0500

    Complete chapter 9 exercises

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -21,6 +21,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
     first = "Foo"
     last  = "Bar"
     email = "foo@bar.com"
